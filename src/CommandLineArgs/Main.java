caca74e1d85f5d3c1fabc381cb0b729db146d1fe
AishwaryaRajLaxmi <aishwarya.laxmi@tothenew.com>
2025-06-24 23:30:52 +0530

    Keep only session 2 assignments in java-session2 branch

--- src/CommandLineArgs/Main.java
+++
@@ -1,9 +0,0 @@
-package CommandLineArgs;
-
-public class Main {
-    public static void main(String args[]){
-        String res=ReadInput.readInput();
-        System.out.println("You entered:" + res);
-
-    }
-}
--- src/noclassdefexception/NoClassDefFoundError.java
+++
@@ -1,8 +0,0 @@
-package noclassdefexception;
-
-public class NoClassDefFoundError {
-    public static void main(String[] args) {
-        Hello h=new Hello();
-        h.sayHello();
-    }
-}
